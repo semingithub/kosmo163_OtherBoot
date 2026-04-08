66fe3fe3717d09a74cf9a5ce4444fce09b1f383c
Hdev-x <261727050+Hdev-x@users.noreply.github.com>
2026-04-08 18:27:16 +0900

    commit

--- src/main/java/com/gguek/app/professor/controller/ProfessorController.java
+++ src/main/java/com/gguek/app/professor/controller/ProfessorController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.gguek.app.professor.dto.ProfessorDTO;
@@ -23,6 +24,41 @@ public class ProfessorController {
 		List<ProfessorDTO> ar = professorService.list();
 		
 		model.addAttribute("list", ar);
+	}
+	
+	@GetMapping("detail")
+	public void detail(Model model, ProfessorDTO professorDTO) throws Exception {
+		professorDTO = professorService.detail(professorDTO);
+		model.addAttribute("detail", professorDTO);		
+	}
+	
+	@GetMapping("create")
+	public void create() {
+		
+	}
+	
+	@PostMapping("create")
+	public String create(ProfessorDTO professorDTO) throws Exception {
+		int result = professorService.create(professorDTO);
+		return "redirect:./list";
+	}
+	
+	@GetMapping("update")
+	public void update(Model model, ProfessorDTO professorDTO) throws Exception {
+		professorDTO = professorService.detail(professorDTO);
+		model.addAttribute("update", professorDTO);
+	}
+	
+	@PostMapping("update")
+	public String update(ProfessorDTO professorDTO) throws Exception {
+		int result = professorService.update(professorDTO);
+		return "redirect:./list";
+	}
+	
+	@PostMapping("delete")
+	public String delete(ProfessorDTO professorDTO) throws Exception {
+		int result = professorService.delete(professorDTO);
+		return "redirect:./list";
 	}
 	
 }
--- src/main/java/com/gguek/app/professor/service/ProfessorService.java
+++ src/main/java/com/gguek/app/professor/service/ProfessorService.java
@@ -8,18 +8,30 @@ import org.springframework.stereotype.Service;
 import com.gguek.app.professor.dto.ProfessorDTO;
 import com.gguek.app.professor.mapper.ProfessorMapper;
 
-
-
 @Service
 public class ProfessorService {
-	
+
 	@Autowired
 	private ProfessorMapper professorMapper;
-	
+
 	public List<ProfessorDTO> list() throws Exception {
 		return professorMapper.list();
 	}
-	
 
-	
+	public ProfessorDTO detail(ProfessorDTO professorDTO) throws Exception {
+		 return professorMapper.detail(professorDTO);
+	}
+
+	public int create(ProfessorDTO professorDTO) throws Exception {
+		return professorMapper.create(professorDTO);
+	}
+
+	public int update(ProfessorDTO professorDTO) throws Exception {
+		return professorMapper.create(professorDTO);
+	}
+
+	public int delete(ProfessorDTO professorDTO) throws Exception {
+		return professorMapper.create(professorDTO);
+	}
+
 }
